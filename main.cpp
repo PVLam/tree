be0485baf898b0652880aa121dd069122fdbf360
lam <lam@ubuntu.ubuntu-domain>
2018-10-03 21:12:11 +0700

    tree in queue

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+#include "tree.h"
+int main()
+{
+    Tree<int> x;
+    x.AddChild(6);
+    x.AddChild(4);
+    x.AddChild(5);
+    x.AddChild(8);
+    x.AddChild(9);
+    x.AddChild(1);
+    x.PreOrder();
+    return 0;
+}
